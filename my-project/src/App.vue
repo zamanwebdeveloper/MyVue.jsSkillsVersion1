fbf59427c980e8ea9a8d263ecba162c5c6283c16
Syed Zaman Mostafiz <zamanwebdeveloper>
2018-09-21 20:02:40 +0600

    17/45 Vue.js 2 Bangla Tutorial #17 Introduction to vue-cli of CSE-425 Project Work Completed.

---
+++ my-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Vue cli</h1>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
