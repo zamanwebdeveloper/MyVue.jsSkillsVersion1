da5156a7672ec414c0895df52f83b97f14767f31
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2018-09-24 00:27:19 +0600

    19/45 Vue.js 2 Bangla Tutorial #19: vue-cli | integrate any template of CSE-425 Project Work Completed.

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,172 +1,173 @@
 <template>
   <div id="all">
 
-      <div class="headersection templete clear">
-        <div class="logo">
-          <img src="/dist/images/logo.png" alt="" />
-          <h2>{{msg}}</h2>
-          <p>Our Website Description</p>
-        </div>
-        <div class="social">
-          <a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-          <a target="_blank" href="http://www.google.com"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-        </div>
+    <div class="headersection templete clear">
+      <div class="logo">
+        <img src="/dist/images/logo.png" alt="" />
+        <h2>{{msg}}</h2>
+        <p>{{des}}</p>
       </div>
-      <div class="navsection templete ">
-        <ul>
-          <li><a id="active" href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="product.html">Product</a>
-            <ul>
-              <li><a href="product01.html">Product One</a></li>
-              <li><a href="product02.html">Product Two</a></li>
-              <li><a href="product03.html">Product Three</a></li>
-              <li><a href="product04.html">Product Four</a></li>
-              <li><a href="product05.html">Product Five</a></li>
-            </ul>
-          </li>
-          <li><a href="privacy.html">Privacy</a></li>
-          <li><a href="dmca.html">DMCA</a></li>
-          <li><a href="contact.html">Contact</a>
-            <ul>
-              <li><a href="contact01.html">Address One</a></li>
-              <li><a href="contact02.html">Address Two</a></li>
-              <li><a href="contact03.html">Address Three</a></li>
-              <li><a href="contact04.html">Address Four</a></li>
-              <li><a href="contact05.html">Address Five</a></li>
-            </ul>
-          </li>
-          <li><a href="item.html">Item</a></li>
-
-        </ul>
+      <div class="social">
+        <a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a target="_blank" href="http://www.google.com"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
       </div>
-
-
-      <div class="contentsection contemplete clear">
-        <div class="maincontent clear">
-          <div class="samepost clear">
-            <h2>Our post title is here</h2>
-            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-            <p>
-              Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
-            </p>
-            <div class="readmore clear">
-              <a href="post.html">Read More</a>
-            </div>
-          </div>
-
-          <div class="samepost clear">
-            <h2>Our post title is here</h2>
-            <a href="post.html"><img src="/dist/images/post2.png" alt="post image" /></a>
-            <p>
-              Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
-            </p>
-            <div class="readmore clear">
-              <a href="post.html">Read More</a>
-            </div>
-          </div>
-
-          <div class="samepost clear">
-            <h2>Our post title is here</h2>
-            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-            <p>
-              Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
-            </p>
-            <div class="readmore clear">
-              <a href="post.html">Read More</a>
-            </div>
-          </div>
-
-
-
-        </div>
-        <div class="sidebar clear">
-          <div class="samesidebar clear">
-            <h2>Latest Articles</h2>
-            <ul>
-              <li><a href="#">Post Title One</a></li>
-              <li><a href="#">Post Title Two</a></li>
-              <li><a href="#">Post Title Three</a></li>
-              <li><a href="#">Post Title Four</a></li>
-              <li><a href="#">Post Title Five</a></li>
-              <li><a href="#">Post Title Six</a></li>
-
-            </ul>
-          </div>
-
-          <div class="samesidebar clear">
-            <h2>Popular Articles</h2>
-            <div class="popular clear">
-              <h3><a href="#">Post title will go here .......</a></h3>
-              <a href="/dist/post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-              <p>
-              <p>
-                Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
-              </p>
-            </div>
-
-            <div class="popular clear">
-              <h3><a href="#">Post title will go here .......</a></h3>
-              <a href="/dist/post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-              <p>
-              <p>
-                Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
-              </p>
-            </div>
-
-            <div class="popular clear">
-              <h3><a href="#">Post title will go here .......</a></h3>
-              <a href="/dist/post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-              <p>
-              <p>
-                Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
-              </p>
-            </div>
-
-            <div class="popular clear">
-              <h3><a href="#">Post title will go here .......</a></h3>
-              <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
-              <p>
-              <p>
-                Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
-              </p>
-            </div>
-          </div>
-
-
-
-        </div>
-      </div>
-      <!--<div style="clear:both"></div>-->
-      <div class="footersection templete clear">
-        <div class="footermenu clear">
+    </div>
+    <div class="navsection templete ">
+      <ul>
+        <li><a id="active" href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="product.html">Product</a>
           <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="privacy.html">Privacy</a></li>
+            <li><a href="product01.html">Product One</a></li>
+            <li><a href="product02.html">Product Two</a></li>
+            <li><a href="product03.html">Product Three</a></li>
+            <li><a href="product04.html">Product Four</a></li>
+            <li><a href="product05.html">Product Five</a></li>
+          </ul>
+        </li>
+        <li><a href="privacy.html">Privacy</a></li>
+        <li><a href="dmca.html">DMCA</a></li>
+        <li><a href="contact.html">Contact</a>
+          <ul>
+            <li><a href="contact01.html">Address One</a></li>
+            <li><a href="contact02.html">Address Two</a></li>
+            <li><a href="contact03.html">Address Three</a></li>
+            <li><a href="contact04.html">Address Four</a></li>
+            <li><a href="contact05.html">Address Five</a></li>
+          </ul>
+        </li>
+        <li><a href="item.html">Item</a></li>
+
+      </ul>
+    </div>
+
+
+    <div class="contentsection contemplete clear">
+      <div class="maincontent clear">
+        <div class="samepost clear">
+          <h2>Our post title is here</h2>
+          <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+          <p>
+            Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
+          </p>
+          <div class="readmore clear">
+            <a href="post.html">Read More</a>
+          </div>
+        </div>
+
+        <div class="samepost clear">
+          <h2>Our post title is here</h2>
+          <a href="post.html"><img src="/dist/images/post2.png" alt="post image" /></a>
+          <p>
+            Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
+          </p>
+          <div class="readmore clear">
+            <a href="post.html">Read More</a>
+          </div>
+        </div>
+
+        <div class="samepost clear">
+          <h2>Our post title is here</h2>
+          <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+          <p>
+            Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here. Some text will be go here.
+          </p>
+          <div class="readmore clear">
+            <a href="post.html">Read More</a>
+          </div>
+        </div>
+
+
+
+      </div>
+      <div class="sidebar clear">
+        <div class="samesidebar clear">
+          <h2>Latest Articles</h2>
+          <ul>
+            <li><a href="#">Post Title One</a></li>
+            <li><a href="#">Post Title Two</a></li>
+            <li><a href="#">Post Title Three</a></li>
+            <li><a href="#">Post Title Four</a></li>
+            <li><a href="#">Post Title Five</a></li>
+            <li><a href="#">Post Title Six</a></li>
+
           </ul>
         </div>
-        <p>&copy; Copyright Training With Live Project.</p>
+
+        <div class="samesidebar clear">
+          <h2>Popular Articles</h2>
+          <div class="popular clear">
+            <h3><a href="#">Post title will go here .......</a></h3>
+            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+            <p>
+            <p>
+              Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
+            </p>
+          </div>
+
+          <div class="popular clear">
+            <h3><a href="#">Post title will go here .......</a></h3>
+            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+            <p>
+            <p>
+              Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
+            </p>
+          </div>
+
+          <div class="popular clear">
+            <h3><a href="#">Post title will go here .......</a></h3>
+            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+            <p>
+            <p>
+              Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
+            </p>
+          </div>
+
+          <div class="popular clear">
+            <h3><a href="#">Post title will go here .......</a></h3>
+            <a href="post.html"><img src="/dist/images/post1.jpg" alt="post image" /></a>
+            <p>
+            <p>
+              Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here. Sidebar text is here.
+            </p>
+          </div>
+        </div>
+
+
+
       </div>
-      <div class="fixedicon clear">
-        <a href="http://www.facebook.com" target="_blank"><img src="/dist/images/fb.png" alt="Facebook" /></a>
-        <a href="http://www.twitter.com" target="_blank"><img src="/dist/images/tw.png" alt="Twitter" /></a>
-        <a href="http://www.google.com" target="_blank"><img src="/dist/images/gl.png" alt="GooglePlus" /></a>
-        <a href="http://www.linkedin.com" target="_blank"><img src="/dist/images/in.png" alt="Linkedin" /></a>
+    </div>
+    <!--<div style="clear:both"></div>-->
+    <div class="footersection templete clear">
+      <div class="footermenu clear">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="privacy.html">Privacy</a></li>
+        </ul>
       </div>
+      <p>&copy; Copyright Training With Live Project.</p>
+    </div>
+    <div class="fixedicon clear">
+      <a href="http://www.facebook.com" target="_blank"><img src="/dist/images/fb.png" alt="Facebook" /></a>
+      <a href="http://www.twitter.com" target="_blank"><img src="/dist/images/tw.png" alt="Twitter" /></a>
+      <a href="http://www.google.com" target="_blank"><img src="/dist/images/gl.png" alt="GooglePlus" /></a>
+      <a href="http://www.linkedin.com" target="_blank"><img src="/dist/images/in.png" alt="Linkedin" /></a>
+    </div>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'all',
+  name: 'app',
   data () {
     return {
-      msg: 'Dream Implementation'
+      msg: 'Dream Implement',
+        des: 'Welcome to our Dream Implementation Method'
     }
   }
 }
